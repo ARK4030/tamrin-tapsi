210493928440803edf61e2c28408bce5dbeea23a
ARK4030 <ali.koveily@gmail.com>
2022-05-28 18:10:54 +0430

    all files was changed

---
+++ JS/main.js
@@ -0,0 +1,53 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+    console.log("clicked");
+    navbar.classList.toggle("nav__expanded");
+});
+
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        const targetTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+        // remove prev actived classes
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+        
+        // add new active classes
+        tab.classList.add("active");
+        targetTabContent.classList.add("active");
+
+    });
+});
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+      const accordion = document.querySelector(".accordion");
+      accordionHeaders.forEach((item) => {
+        item.addEventListener("click", (e) => {
+          console.log(e.target.parentElement);
+          e.target.parentElement.classList.toggle("accordion__expanded");
+        });
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
